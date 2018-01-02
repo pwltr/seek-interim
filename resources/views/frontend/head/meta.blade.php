79045b2a5db2bf37378609f9f330609ab64eb029
philippwalter <walter@abss.at>
2018-01-02 22:14:23 +0100

    update meta tags

--- resources/views/frontend/head/meta.blade.php
+++ resources/views/frontend/head/meta.blade.php
@@ -1,7 +1,21 @@
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<title>SEEK | Welcome</title>
+<title>Seek Athletics</title>
 <meta name="description" content="" />
 <meta name="keywords" content="" />
 <meta name="author" content="SEEK Web Department" />
+
+<meta name="Title" content="Seek Athletics"/>
+<meta name="Author" content="Seek Athletics"/>
+<meta name="Publisher" content="Seek Athletics"/>
+<meta name="Copyright" content="Seek Athletics"/>
+<meta name="Revisit" content="After 10 days"/>
+<meta name="Keywords" content="sports, clothing, outdoor, urban"/>
+<meta name="Description" content="Lasse dich über Neuigkeiten zum Produktstart von Seek Athletics informieren!"/>
+<meta name="Abstract" content="Lasse dich über Neuigkeiten zum Produktstart von Seek Athletics informieren!"/>
+<meta name="page-topic" content="Sport"/>
+<meta name="page-topic" content="Produktinfo"/>
+<meta name="audience" content="Männer"/>
+<meta name="Robots" content="INDEX,FOLLOW"/>
+<meta name="Language" content="Deutsch"/>
