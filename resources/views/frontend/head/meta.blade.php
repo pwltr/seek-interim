3604db9de2094e9b20d81ee0ce3ce33c6dea358e
philippwalter <walter@abss.at>
2018-01-07 18:09:05 +0100

    redirect to https, update meta info

--- resources/views/frontend/head/meta.blade.php
+++ resources/views/frontend/head/meta.blade.php
@@ -1,21 +1,17 @@
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<title>Seek Athletics</title>
-<meta name="description" content="" />
-<meta name="keywords" content="" />
-<meta name="author" content="SEEK Web Department" />
-
-<meta name="Title" content="Seek Athletics"/>
-<meta name="Author" content="Seek Athletics"/>
-<meta name="Publisher" content="Seek Athletics"/>
-<meta name="Copyright" content="Seek Athletics"/>
-<meta name="Revisit" content="After 10 days"/>
-<meta name="Keywords" content="sports, clothing, outdoor, urban"/>
-<meta name="Description" content="Lasse dich über Neuigkeiten zum Produktstart von Seek Athletics informieren!"/>
-<meta name="Abstract" content="Lasse dich über Neuigkeiten zum Produktstart von Seek Athletics informieren!"/>
+<title>SEEK Athletics</title>
+<meta name="Title" content="SEEK Athletics"/>
+<meta name="Author" content="Philipp Walter" />
+<meta name="Publisher" content="Christian Fleck"/>
+<meta name="Copyright" content="Christian Fleck"/>
+<meta name="Revisit" content="After 30 days"/>
+<meta name="Keywords" content="Underwear men, multisport, progressive sportswear, sports brand, no roll up underwear, clothing, sports clothing"/>
+<meta name="Description" content="Underwear that does not roll up"/>
+<meta name="Abstract" content="Underwear that does not roll up"/>
 <meta name="page-topic" content="Sport"/>
 <meta name="page-topic" content="Produktinfo"/>
-<meta name="audience" content="Männer"/>
+<meta name="audience" content="Erwachsene"/>
 <meta name="Robots" content="INDEX,FOLLOW"/>
 <meta name="Language" content="Deutsch"/>
